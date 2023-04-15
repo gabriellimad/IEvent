fb82c08023c2c47ea6366f120b0092761f026b09
Gabriel Lima De Almeida Silva <gabriel.almeida@rte.com.br>
2023-04-15 18:19:10 -0300

    Adição das telas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,117 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
 import 'package:flutter/material.dart';
+import 'package:ievent/view/Sobrenos.dart';
+import 'package:ievent/view/widgets.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'tela principal',
+    home: TelaPrincipal(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class TelaPrincipal extends StatelessWidget {
+  const TelaPrincipal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    final screenHeight = MediaQuery.of(context).size.height;
+    final containerHeight =
+        screenHeight * 0.7 - 120; // subtrai a altura da imagem do EzPrice
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(51, 24, 117, 1),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('images/gradMorpheu.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(),
+          ),
+          Positioned(
+            top: screenHeight * 0.1,
+            left: 0,
+            right: 0,
+            child: Container(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'images/ezprice-removebg-preview.png',
+                height: 120,
+              ),
+            ),
+          ),
+          Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 90, vertical: 50),
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(30, 0, 30, 3),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(0, 0, 0, 0.3),
+                      borderRadius: BorderRadius.all(Radius.circular(30)),
+                      border: Border.all(
+                          color: Colors.black.withOpacity(0.4), width: 1.5)),
+                  height: containerHeight,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Widgets().widgetEmail(),
+                        SizedBox(height: 10),
+                        Widgets().widgetSenha(),
+                        SizedBox(height: 2),
+                        Row(
+                          children: [Widgets().widgetEsqueceSenha(context)],
+                          mainAxisAlignment: MainAxisAlignment.end,
+                        ),
+                        SizedBox(height: 10),
+                        Widgets().widgetEntrar(context),
+                        SizedBox(height: 25),
+                        Widgets().widgetNovoAqui(context),
+                      ])),
+            ),
+          ])
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('images/gradMorpheu.png'),
+                  fit: BoxFit.cover)),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountEmail: Text('lucasvidotto12@gmail.com'),
+                accountName: Text('Lucas Vidotto'),
+                decoration:
+                    BoxDecoration(color: Color.fromRGBO(51, 24, 117, 1)),
+                currentAccountPicture: CircleAvatar(
+                  child: Text(
+                    'UN',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  backgroundColor: Color.fromRGBO(51, 24, 117, 0.7),
+                ),
+              ),
+              ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text('Sobre nós'),
+                  tileColor: Colors.black,
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Sobrenos()));
+                  })
+            ],
+          ),
         ),
       ),
     );
